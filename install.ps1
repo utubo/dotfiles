da7c6b25f4f4fa3159a4f77830492c66f599dc0d
utubo <utbutb@gmail.com>
2023-04-09 00:25:33 +0900

    fix windows path

--- install.ps1
+++ install.ps1
@@ -1,12 +1,9 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) { Start-Process powershell.exe "-File `"$PSCommandPath`"" -Verb RunAs; exit }
 
 New-Item -ItemType SymbolicLink -Value $PSScriptRoot/.gitignore_global -Path ~/.gitignore_global
-New-Item -ItemType SymbolicLink -Value $PSScriptRoot/.gvimrc -Path ~/.gvimrc
-New-Item -ItemType SymbolicLink -Value $PSScriptRoot/.vimrc -Path ~/.vimrc
+New-Item -ItemType SymbolicLink -Value $PSScriptRoot/.gvimrc -Path ~/_gvimrc
+New-Item -ItemType SymbolicLink -Value $PSScriptRoot/.vimrc -Path ~/_vimrc
 New-Item -ItemType SymbolicLink -Value $PSScriptRoot/.vsnip -Path ~/.vsnip
-New-Item -ItemType SymbolicLink -Value $PSScriptRoot/.zlogin -Path ~/.zlogin
-New-Item -ItemType SymbolicLink -Value $PSScriptRoot/.zlogout -Path ~/.zlogout
-New-Item -ItemType SymbolicLink -Value $PSScriptRoot/.zshrc -Path ~/.zshrc
 mkdir -p ~/vimfiles
 New-Item -ItemType SymbolicLink -Value $PSScriptRoot/.vim/after -Path ~/vimfiles/after
 
